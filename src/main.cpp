e3885bc928db9e01951497de216091b9707df9f3
riban <brian@riban.co.uk>
2024-01-01 22:25:11 +0000

    Use 32-bit magic number to check for intialised EEPROM. Use sprite as double buffer to remove screen flicker. Implement sliding buttons for screen timeout and brightness. Use proper alignment for text. All screen drawing now done in refresh at 50Hz. Show battery level - turns blue when BLE connected.

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <BLEMidi.h> // Provides BLE MIDI interface
 #include <EEPROM.h>
 
+#define MAGIC 0x7269626e // Used to check if EEPROM has been initialised
+
 enum mode_enum {
     MODE_XY,
     MODE_PADS,
@@ -52,14 +54,18 @@ enum setting_enum {
     SETTINGS_TIMEOUT
 };
 
-TTGOClass *ttgo; // Pointer to singleton instance of ttgo watch object
-BMA *accel; // Pointer to accelerometer sensor
+TTGOClass* ttgo; // Pointer to singleton instance of ttgo watch object
+BMA* accel; // Pointer to accelerometer sensor
+TFT_eSprite* sprite; // Pointer to sprite acting as display double buffer
 
 class gfxButton {
     public:
         gfxButton(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t bg, uint32_t bgh, const char* text=nullptr, uint8_t mode=MODE_NONE) :
             m_x(x), m_y(y), m_w(w), m_h(h), m_bg(bg), m_bgh(bgh), m_mode(mode) {
+                m_fg = TFT_WHITE;
                 m_rad = m_h / 4;
+                m_indent_x = m_w / 2;
+                m_indent_y = m_h / 2;
                 if (text) {
                     m_text = (char*)malloc(strlen(text));
                     sprintf(m_text, text, strlen(text));
@@ -67,12 +73,24 @@ class gfxButton {
             };
 
         void draw(bool hl=false) {
-            ttgo->tft->fillRoundRect(m_x, m_y, m_w, m_h, m_rad, hl?m_bgh:m_bg);
+            sprite->fillRoundRect(m_x, m_y, m_w, m_h, m_rad, hl?m_bgh:m_bg);
             if (m_text) {
-                ttgo->tft->setTextColor(TFT_WHITE);
-                ttgo->tft->setTextFont(4);
-                ttgo->tft->setCursor(m_x + m_indent_x, m_y + m_indent_y);
-                ttgo->tft->print(m_text);
+                sprite->setTextColor(m_fg);
+                sprite->setTextDatum(m_align);
+                sprite->drawString(m_text, m_x + m_indent_x, m_y + m_indent_y, 4);
+                sprite->setTextDatum(TL_DATUM);
+            }
+        }
+
+        void drawBar(uint16_t percent) {
+            uint16_t x = percent * m_w / 100;
+            sprite->fillRoundRect(m_x, m_y, m_w, m_h, m_rad, m_bg);
+            sprite->fillRoundRect(m_x, m_y, x, m_h, m_rad, m_bgh);
+            if (m_text) {
+                sprite->setTextColor(m_fg);
+                sprite->setTextDatum(m_align);
+                sprite->drawString(m_text, m_x + m_indent_x, m_y + m_indent_y, 4);
+                sprite->setTextDatum(TL_DATUM);
             }
         }
 
@@ -84,23 +102,24 @@ class gfxButton {
             return m_mode;
         }
 
-    uint32_t m_bg, m_bgh;
+    uint32_t m_bg, m_bgh, m_fg;
     uint16_t m_x, m_y, m_w, m_h;
     uint8_t m_rad;
     uint8_t m_mode = MODE_NONE;
-    uint8_t m_indent_x = 10;
-    uint8_t m_indent_y = 10;
+    uint8_t m_indent_x;
+    uint8_t m_indent_y;
+    uint8_t m_align = MC_DATUM;
     char * m_text = nullptr;
 };
 
 gfxButton* settingsBtns[] = {
-    new gfxButton(20, 0, 200, 58, 0x12748c, TFT_OLIVE, "BLE", MODE_BLE),
-    new gfxButton(20, 60, 200, 58, 0x12748c, TFT_OLIVE, "MIDI CHAN", MODE_MIDICHAN),
-    new gfxButton(20, 120, 200, 58, 0x12748c, TFT_OLIVE, "X-CC", MODE_CCX),
-    new gfxButton(20, 180, 200, 58, 0x12748c, TFT_OLIVE, "Y-CC", MODE_CCY),
-    new gfxButton(20, 240, 200, 58, 0x12748c, TFT_OLIVE, "Rx-CC", MODE_CCRX),
-    new gfxButton(20, 300, 200, 58, 0x12748c, TFT_OLIVE, "Brightness", MODE_BRIGHTNESS),
-    new gfxButton(20, 300, 200, 58, 0x12748c, TFT_OLIVE, "Screen time", MODE_TIMEOUT)};
+    new gfxButton(20, 0, 200, 58, TFT_DARKGREY, TFT_LIGHTGREY, "BLE", MODE_BLE),
+    new gfxButton(20, 60, 200, 58, TFT_DARKGREY, TFT_LIGHTGREY, "MIDI CHAN", MODE_MIDICHAN),
+    new gfxButton(20, 120, 200, 58, TFT_DARKGREY, TFT_LIGHTGREY, "X-CC", MODE_CCX),
+    new gfxButton(20, 180, 200, 58, TFT_DARKGREY, TFT_LIGHTGREY, "Y-CC", MODE_CCY),
+    new gfxButton(20, 240, 200, 58, TFT_DARKGREY, TFT_LIGHTGREY, "Rx-CC", MODE_CCRX),
+    new gfxButton(20, 300, 200, 58, TFT_DARKGREY, TFT_LIGHTGREY, "Brightness", MODE_BRIGHTNESS),
+    new gfxButton(20, 300, 200, 58, TFT_DARKGREY, TFT_LIGHTGREY, "Screen time", MODE_TIMEOUT)};
 gfxButton* launchPads[16];
 gfxButton* numPad[11];
 
@@ -112,6 +131,7 @@ uint8_t selPad = -1; // Index of selected pad
 uint8_t oskSel = MODE_NONE; // Index of button selected on touch screen
 uint8_t settingsOffset = 0; // Settings view scroll position
 uint8_t crosshair_x, crosshair_y; // Coordinates of X-Y controller crosshairs
+uint8_t battery; // Battery %
 volatile uint32_t screenTimeout = 0; // Countdown timer until auto standby mode
 uint32_t now = 0; // Time of current loop process
 bool standby = true; // True if in standby mode (screen off)
@@ -137,40 +157,49 @@ void numEntry();
 // Initialisation
 void setup(void)
 {
-    EEPROM.begin(eepromSize + 1);
-    if(EEPROM.read(eepromSize) == 49)
-        EEPROM.readBytes(0, settings, eepromSize);
     Serial.begin(115200); // Can use USB for debug
 
-    if (settings[SETTING_BLE])
-        startBle();
+    ttgo = TTGOClass::getWatch(); // Create instance of watch object (singleton)
+    ttgo->begin(); // Initialise watch object
+    sprite = new TFT_eSprite(ttgo->tft);
+    sprite->createSprite(240, 240);
 
+    EEPROM.begin(eepromSize + 4);
+    uint32_t magic;
+    EEPROM.readBytes(0, &magic, 4);
+    if (magic == MAGIC) {
+        EEPROM.readBytes(4, settings, eepromSize);
+        ttgo->setBrightness(settings[SETTING_BRIGHTNESS]);
+    }
+
+    // Configure settings menu buttons
+    for (uint8_t i = 0; i < 7; ++i) {
+        gfxButton* btn = settingsBtns[i];
+        btn->m_align = ML_DATUM;
+        btn->m_indent_x = 10;
+    }
+
+    // Build launchpad grid
     for (uint8_t col = 0; col < 4; ++col) {
         for (uint8_t row = 0; row < 4; ++row) {
             launchPads[row + 4 * col] = new gfxButton(col * 60, row * 60, 58, 58, TFT_DARKGREEN, TFT_GREEN);
         }
     }
+
     // Build numeric keyapd
     numPad[0] = new gfxButton(0, 0, 78, 58, TFT_DARKGREY, TFT_LIGHTGREY, "0", 0);
-    numPad[0]->m_indent_x = 32;
-    numPad[0]->m_indent_y = 18;
     char txt[2];
     for (uint8_t col = 0; col < 3; ++col) {
         for (uint8_t row = 0; row < 3; ++row) {
             uint8_t i = col + row * 3 + 1;
             sprintf(txt, "%d", i);
             numPad[i] = new gfxButton(col * 80, 60 + row * 60, 78, 58, TFT_DARKGREY, TFT_LIGHTGREY, txt, i);
-            numPad[i]->m_indent_x = 32;
-            numPad[i]->m_indent_y = 18;
         }
     }
-    numPad[10] = new gfxButton(80, 0, 158, 78, TFT_LIGHTGREY, TFT_DARKGREY, "   ", 10);
-    numPad[10]->m_indent_x = 50;
-    numPad[10]->m_indent_y = 18;
+    numPad[10] = new gfxButton(80, 0, 158, 58, TFT_LIGHTGREY, TFT_DARKGREY, "   ", 10);
+    numPad[10]->m_fg = TFT_BLACK;
 
-    ttgo = TTGOClass::getWatch(); // Create instance of watch object (singleton)
-    ttgo->begin(); // Iniitalise watch object
-
+    // Initialise haptic feedback motor
     ttgo->motor_begin();
 
     // Configure power button
@@ -178,8 +207,6 @@ void setup(void)
     attachInterrupt(AXP202_INT, [] {
         irq = true;
     }, FALLING);
-
-    //!Clear IRQ unprocessed first
     ttgo->power->enableIRQ(AXP202_PEK_SHORTPRESS_IRQ | AXP202_PEK_LONGPRESS_IRQ| AXP202_VBUS_REMOVED_IRQ | AXP202_VBUS_CONNECT_IRQ | AXP202_CHARGING_IRQ, true);
     ttgo->power->clearIRQ();
 
@@ -192,6 +219,9 @@ void setup(void)
     cfg.perf_mode = BMA4_CONTINUOUS_MODE;
     accel->accelConfig(cfg);
     accel->enableAccel();
+
+    if (settings[SETTING_BLE])
+        startBle();
 
     screenOn();
 }
@@ -233,19 +263,18 @@ void loop()
         // 1ms (or slower)
         processTouch();
         processAccel();
+        if (!standby && now > lastNow + 20)
+            refresh();
     }
 
-    if (pulseRadius && (nextPulse < now)) {
+    if (pulseRadius && (nextPulse < now))
         nextPulse = now + 6;
-        ttgo->tft->drawCircle(120, 120, lastPulseRadius, TFT_BLACK);
-        lastPulseRadius = pulseRadius;
-        ttgo->tft->drawCircle(120, 120, pulseRadius--, TFT_DARKCYAN);
-    }
 
     if (nextSecond < now) {
         nextSecond += 1000;
         if (screenTimeout && (--screenTimeout == 0))
             screenOff();
+        battery = ttgo->power->getBattPercentage();
         blink = !blink;
     }
 
@@ -279,17 +308,13 @@ void processTouch() {
                         if (settings[SETTING_BLE])
                             BLEMidiServer.controlChange(settings[SETTING_MIDICHAN], settings[SETTING_CCX], cc_x);
                         last_cc_x = cc_x;
-                        ttgo->tft->drawLine(crosshair_x, 0, crosshair_x, 240, TFT_BLACK);
                         crosshair_x = x;
-                        ttgo->tft->drawLine(crosshair_x, 0, crosshair_x, 240, TFT_RED);
                     }
                     if (cc_y != last_cc_y) {
                         if (settings[SETTING_BLE])
                             BLEMidiServer.controlChange(settings[SETTING_MIDICHAN], settings[SETTING_CCY], cc_y);
                         last_cc_y = cc_y;
-                        ttgo->tft->drawLine(0, crosshair_y, 240, crosshair_y, TFT_BLACK);
                         crosshair_y = y;
-                        ttgo->tft->drawLine(0, crosshair_y, 240, crosshair_y, TFT_RED);
                     }
                     break;
                 case MODE_PADS:
@@ -297,19 +322,35 @@ void processTouch() {
                     uint8_t pad = y / 60 + x / 60 * 4;
                     if (pad < 16) {
                         if (pad != selPad) {
-                            if (selPad < 16) {
+                            if (selPad < 16)
                                 BLEMidiServer.noteOn(settings[SETTING_MIDICHAN], 48 + selPad, 0);
-                                launchPads[selPad]->draw();
-                            }
                             BLEMidiServer.noteOn(settings[SETTING_MIDICHAN], 48 + pad, 100);
                             selPad = pad;
-                            launchPads[selPad]->draw(true);
                         }
                     }
                     break;
                 }
                 case MODE_SETTINGS:
                 {
+                    if (settingsBtns[SETTING_BRIGHTNESS]->bounds(x, y)) {
+                        int16_t dX = x - startX;
+                        if (dX > 5 || dX < -5) {
+                            // A bit of hysteresis
+                            int16_t val = (x - settingsBtns[SETTING_BRIGHTNESS]->m_x) * 255 / settingsBtns[SETTING_BRIGHTNESS]->m_w;
+                            settings[SETTING_BRIGHTNESS] = val;
+                            ttgo->setBrightness(val);
+                            startX = x;
+                        }
+                    }
+                    else if (settingsBtns[SETTINGS_TIMEOUT]->bounds(x, y)) {
+                        int16_t dX = x - startX;
+                        if (dX > 5 || dX < -5) {
+                            // A bit of hysteresis
+                            int16_t val = (x - settingsBtns[SETTINGS_TIMEOUT]->m_x) * 120 / settingsBtns[SETTINGS_TIMEOUT]->m_w;
+                            settings[SETTINGS_TIMEOUT] = val;
+                            startX = x;
+                        }
+                    }
                     int16_t dY = y - startY;
                     if(!scrolling) {
                         if (dY > 10 || dY < -10)
@@ -325,21 +366,10 @@ void processTouch() {
                             else
                                 settingsOffset = uint8_t(dY);
                             startY = y;
-                            refresh();
                         }
                     }
                     break;
                 }
-                case MODE_BRIGHTNESS:
-                    int16_t dY = y - startY;
-                    if (dY < 10 && dY > -10 || now < lastUpdateTime + 100)
-                        break;
-                    settings[SETTING_BRIGHTNESS] = y;
-                    ttgo->setBrightness(uint8_t(255 - y));
-                    ttgo->tft->fillRoundRect(80, 0, 120, 240, 10, TFT_DARKGREY);
-                    ttgo->tft->fillRoundRect(80, y, 120, 240, 10, TFT_WHITE);
-                    lastUpdateTime = now;
-                    break;
             }
             lastX = x;
             lastY = y;
@@ -357,15 +387,12 @@ void processTouch() {
         switch (mode) {
             case MODE_PADS:
                 BLEMidiServer.noteOn(settings[SETTING_MIDICHAN], 48 + selPad, 0);
-                if (selPad < 16)
-                    launchPads[selPad]->draw();
                 selPad = -1;
                 break;
             case MODE_MIDICHAN:
             case MODE_CCX:
             case MODE_CCY:
             case MODE_CCRX:
-            case MODE_TIMEOUT:
                 // Handle keypad release
                 for (uint8_t i = 0; i < 11; ++i) {
                     gfxButton* btn = numPad[i];
@@ -386,11 +413,12 @@ void processTouch() {
                     if (mode == MODE_BLE) {
                         toggleBle();
                         mode = MODE_SETTINGS;
+                    } else if (mode == MODE_BRIGHTNESS || mode == MODE_TIMEOUT) {
+                        mode = MODE_SETTINGS;
                     }
-                    refresh();
-                        break;
+                    break;
+                }
                 break;
-            }
         }
     oskSel = MODE_NONE;
     }
@@ -424,9 +452,9 @@ bool processAccel() {
 
     if (accel->getAccel(acc)) {
         /*
-        ttgo->tft->setTextFont(4);
-        ttgo->tft->setCursor(40, 100);
-        ttgo->tft->printf("%03d, %03d, %03d", acc.x, acc.y, acc.z);
+        sprite->setTextFont(4);
+        sprite->setCursor(40, 100);
+        sprite->printf("%03d, %03d, %03d", acc.x, acc.y, acc.z);
         */
         return true;
     } else
@@ -479,8 +507,9 @@ void onPowerButtonShortPress() {
             break;
         default:
             if (mode == MODE_SETTINGS) {
-                EEPROM.writeBytes(0, settings, eepromSize);
-                EEPROM.write(eepromSize, 49);
+                uint32_t magic = MAGIC;
+                EEPROM.writeBytes(0, &magic, 4);
+                EEPROM.writeBytes(4, settings, eepromSize);
                 EEPROM.commit();
             }
             if(++mode > MODE_PADS) {
@@ -502,53 +531,64 @@ void onPowerButtonLongPress() {
 }
 
 void refresh() {
-    ttgo->tft->fillScreen(TFT_BLACK); // Clear screen
-    ttgo->tft->setTextColor(TFT_WHITE, TFT_BLACK);  // Adding a background colour erases previous text automatically
+    sprite->fillScreen(TFT_BLACK); // Clear screen
+    sprite->setTextColor(TFT_WHITE);  // Adding a background colour erases previous text automatically
+    sprite->setTextFont(4);
     switch(mode) {
         case MODE_XY:
-            ttgo->tft->drawLine(crosshair_x, 0, crosshair_x, 240, TFT_RED);
-            ttgo->tft->drawLine(0, crosshair_y, 240, crosshair_y, TFT_RED);
+            sprite->drawLine(crosshair_x, 0, crosshair_x, 240, TFT_RED);
+            sprite->drawLine(0, crosshair_y, 240, crosshair_y, TFT_RED);
+            if (pulseRadius)
+                sprite->drawCircle(120, 120, pulseRadius--, TFT_DARKCYAN);
             break;
         case MODE_PADS:
             for (uint8_t pad = 0; pad < 16; ++pad)
                 launchPads[pad]->draw(selPad == pad);
             break;
         case MODE_SETTINGS:
-            ttgo->tft->fillScreen(TFT_BLACK);
             for (uint8_t i = 0; i < 4; ++i) {
                 uint8_t j = settingsOffset + i;
                 gfxButton* btn = settingsBtns[j];
                 btn->m_y = i * 60;
-                btn->draw();
-                ttgo->tft->setCursor(170, 10 + 60 * i);
-                if (j == 0)
-                    ttgo->tft->printf("%s", settings[SETTING_BLE]?"ON":"OFF");
-                else if (j == 1)
-                    ttgo->tft->printf("%d", settings[j] + 1);
+                if (j == SETTING_BRIGHTNESS)
+                    btn->drawBar(100 * settings[SETTING_BRIGHTNESS] / 255);
+                else if (j == SETTINGS_TIMEOUT)
+                    btn->drawBar(100 * settings[SETTINGS_TIMEOUT] / 120);
                 else
-                    ttgo->tft->printf("%d", settings[j]);
+                    btn->draw();
+                sprite->setTextDatum(MR_DATUM);
+                int32_t x = 210;
+                int32_t y = 29 + 60 * i;
+                if (j == 0)
+                    if (settings[SETTING_BLE])
+                        sprite->drawString("ON", x, y, 4);
+                    else
+                        sprite->drawString("OFF", x, y, 4);
+                else if (j == 1)
+                    sprite->drawNumber(settings[j] + 1, x, y, 4);
+                else
+                    sprite->drawNumber(settings[j], x, y, 4);
+                sprite->setTextDatum(TL_DATUM);
             }
             break;
         case MODE_MIDICHAN:
         case MODE_CCX:
         case MODE_CCY:
         case MODE_CCRX:
-        case MODE_TIMEOUT:
             // Draw numeric keypad
-            ttgo->tft->fillScreen(TFT_BLACK);
-            for (uint8_t i = 0; i < 10; ++i)
+            for (uint8_t i = 0; i < 11; ++i)
                 numPad[i]->draw();
-            oskSel = 10;
-            numEntry();
             break;
     }
     showStatus();
+    sprite->pushSprite(0, 0);
 }
 
 void showStatus() {
-    ttgo->tft->fillRect(180, 0, 239, 1, settings[SETTING_BLE] && BLEMidiServer.isConnected()?TFT_BLUE:TFT_BLACK);
-    if (mode == MODE_PADS && !BLEMidiServer.isConnected())
-        launchPads[12]->draw();
+    sprite->setTextFont(2);
+    sprite->setCursor(200, 0);
+    sprite->setTextColor(BLEMidiServer.isConnected()?TFT_BLUE:TFT_WHITE);
+    sprite->printf("%d%%", battery);
 }
 
 void startBle() {
@@ -585,30 +625,24 @@ void numEntry() {
         if (offset == 0 && v > 1 || offset == 1 && (v < 0 || v > 16))
             return;
         oMax = 2;
-    } else if (mode == MODE_TIMEOUT) {
-        if (offset == 0 && v > 2 || offset == 1 && v >25 || offset == 2 && v > 255)
-            return;
-        oMax = 3;
     } else {
         if (offset == 0 && v > 1 || offset == 1 && v > 12 || offset == 2 && v > 127)
             return;
         oMax = 3;
     }
 
-    ttgo->tft->fillRoundRect(80, 2, 158, 58, 10, TFT_LIGHTGREY);
-    ttgo->tft->setCursor(180 - 20 * oMax, 18);
-    ttgo->tft->setTextColor(TFT_BLACK);
-
-    ttgo->tft->fillRoundRect(80, 2, 158, 58, 10, TFT_LIGHTGREY);
-
+    char s[10];
     if (oskSel < 10) {
+        // Create numeric sting from current value
         char fmt[5];
         sprintf(fmt, "%%0%dd", ++offset);
-        ttgo->tft->printf(fmt, v);
+        sprintf(s, fmt, v);
     }
 
     for (uint8_t i = 0; i < oMax - offset; ++i)
-        ttgo->tft->printf(" _");
+        sprintf(s + offset + i * 2, " _");
+
+    sprintf(numPad[10]->m_text, s);
 
     if (offset >= oMax) {
         offset = 0;
@@ -619,9 +653,10 @@ void numEntry() {
         } else {
             settings[mode - MODE_BLE] = v;
         }
-        mode = MODE_SETTINGS;
-        delay(300);
         refresh();
+        delay(300);
+        mode = MODE_SETTINGS;
+        sprintf(numPad[10]->m_text, "");
     } else {
         val = v;
     }
